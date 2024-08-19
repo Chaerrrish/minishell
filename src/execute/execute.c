3bbfa64df4eb37d4c1aa2f0ddc2b383cae662494
chaerrrish <95009493+Chaerrrish@users.noreply.github.com>
2024-08-19 20:10:12 +0900

    Add execute()

--- src/execute/execute.c
+++ src/execute/execute.c
@@ -6,9 +6,56 @@
 /*   By: chaoh <chaoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/18 18:16:18 by chaoh             #+#    #+#             */
-/*   Updated: 2024/08/19 15:56:21 by chaoh            ###   ########.fr       */
+/*   Updated: 2024/08/19 19:41:26 by chaoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/minishell.h"
 
+void	execute(t_shell	*shell)
+{
+	t_cmd_list	*current;
+
+	heredoc(shell->cmd_list);
+	current = shell->cmd_list;
+	while (current)
+	{
+		if (current->next == NULL && current->token_list->type == T_BULTIN)
+			execute_builtin(shell);
+		else
+			execute_cmd(current, shell->env_list);
+		current = current->next;
+	}
+}
+
+void	execute_cmd(t_cmd_list *cmd, t_shell *shell)
+{
+	if (cmd->next != NULL)
+		cmd->pid = fork();
+	if (cmd->pid == -1)
+		return ;
+	else if (cmd->pid == 0)
+	{
+		if (cmd->token_list->type == T_BULTIN)
+		{
+			execute_builtin(shell);
+			exit(0);
+		}
+		else if (cmd->token_list->type == T_WORD)
+			execute_child(cmd, shell->env_list);
+	}
+	else
+	{
+		waitpid(cmd->pid, NULL, 0);
+	}
+}
+
+void	execute_child(t_cmd_list *cmd, t_list *env_list)
+{
+	if (cmd->token_list->type == T_WORD)
+	{
+		set_cmd_path(cmd, env_list);
+		if (execve(cmd->path, cmd->argv[0], env_list) < 0)
+			return ;
+	}
+}
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: chaoh <chaoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/30 19:15:06 by wonyocho          #+#    #+#             */
-/*   Updated: 2024/08/19 17:04:24 by wonyocho         ###   ########.fr       */
+/*   Updated: 2024/08/19 19:14:40 by chaoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ static void	minishell(char **envp)
 	{
 		fd_backup[0] = dup(STDIN_FILENO);	// 표준 입력 백업
 		fd_backup[1] = dup(STDOUT_FILENO);	// 표준 출력 백업
-		input = readline("tontoshell🌝 $ ");
+		input = readline("tontoshell ༼⍤༽ $ ");
 		if (!input) // EOF(Ctrl + D) or readline 오류시 에러핸들링
 			break;
 		if (ft_strlen(input) == 0)
