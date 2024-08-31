979387a52547944548ac0671934ed24463dc20d1
chaerrrish <95009493+Chaerrrish@users.noreply.github.com>
2024-08-31 14:44:48 +0900

    [Fix] builtins 실행할 때 ft_strncmp로 변경

--- src/builtins/builtins.c
+++ src/builtins/builtins.c
@@ -6,7 +6,7 @@
 /*   By: chaoh <chaoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/18 18:26:10 by chaoh             #+#    #+#             */
-/*   Updated: 2024/08/29 18:05:01 by chaoh            ###   ########.fr       */
+/*   Updated: 2024/08/31 14:42:54 by chaoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,22 @@ int	execute_builtin(t_shell *minishell, t_cmd_list *cmd, char **envp)
 	redirection(cmd);
 	if (cmd->out_fd == STDOUT_FILENO && cmd->in_fd == STDIN_FILENO)
 		set_pipe_inout(cmd);
-	if (ft_strcmp(cmd->argv[0], "cd") == 0)
+	if (ft_strncmp(cmd->argv[0], "cd", 2) == 0)
 		cd(cmd, minishell->env_list);
-	else if (!(ft_strcmp(cmd->argv[0], "echo")) \
-			|| ft_strcmp(cmd->argv[0], "ECHO") == 0)
+	else if (!(ft_strncmp(cmd->argv[0], "echo", 4)) \
+			|| ft_strncmp(cmd->argv[0], "ECHO", 4) == 0)
 		ft_echo(cmd);
-	else if (!(ft_strcmp(cmd->argv[0], "env")) \
-			|| ft_strcmp(cmd->argv[0], "ENV") == 0)
+	else if (!(ft_strncmp(cmd->argv[0], "env", 3)) \
+			|| ft_strncmp(cmd->argv[0], "ENV", 3) == 0)
 		env(cmd, minishell->env_list);
-	else if (ft_strcmp(cmd->argv[0], "exit") == 0)
+	else if (ft_strncmp(cmd->argv[0], "exit", 4) == 0)
 		ft_exit(cmd);
-	else if (ft_strcmp(cmd->argv[0], "export") == 0)
+	else if (ft_strncmp(cmd->argv[0], "export", 6) == 0)
 		export(cmd, minishell->env_list, envp);
-	else if (!(ft_strcmp(cmd->argv[0], "pwd")) \
-			|| ft_strcmp(cmd->argv[0], "PWD") == 0)
+	else if (!(ft_strncmp(cmd->argv[0], "pwd", 3)) \
+			|| ft_strncmp(cmd->argv[0], "PWD", 3) == 0)
 		pwd(cmd);
-	else if (ft_strcmp(cmd->argv[0], "unset") == 0)
+	else if (ft_strncmp(cmd->argv[0], "unset", 5) == 0)
 		unset(cmd, &minishell->env_list);
 	else
 		return (0);
